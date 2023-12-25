0df391d7eb6e68e5083a016d188225a1acc565e9
Lavanyasg9455 <146079855+Lavanyasg9455@users.noreply.github.com>
2023-12-25 19:08:09 +0530

    Delete vaccineApp directory

--- vaccineApp/src/main/java/com/tap/vaccine/dao/RegisterDAO.java
+++
@@ -1,9 +0,0 @@
-package com.tap.vaccine.dao;
-
-import com.tap.vaccine.entity.RegisterEntity;
-
-public interface RegisterDAO {
-
-	boolean saveRegisterEntity(RegisterEntity entity);
-	boolean emailCheck(String emailId);
-}
